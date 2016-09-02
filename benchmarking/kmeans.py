73956f460f4b419937b0a6e08101610c6fec816c
Paul Sohn <pksohn@gmail.com>
2016-09-01 22:50:40 -0700

    Benchmarking script with argument passing

--- benchmarking/kmeans.py
+++ benchmarking/kmeans.py
@@ -1,61 +1,61 @@
 import pandas as pd
 import os
-import sys
 import time
 import multiprocessing
 from sklearn.cluster import KMeans
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--start')
+parser.add_argument('--stop')
+parser.add_argument('--step')
+parser.add_argument('--timeout')
+args = parser.parse_args()
+
 
 project = os.path.realpath('./..')
 datadir = os.path.join(project, 'data')
 
 with pd.HDFStore(os.path.join(datadir, 'tweets_1M.h5')) as store:
     subset = store.tweets_subset
-
-# Convert the lat and lng columns to numpy array
 data = subset.as_matrix(columns=['lat', 'lng'])
 
 
 def kmean_fit_timing(data, n):
 
-    k_means = KMeans(n_clusters=n,
-                     init='k-means++',
-                     n_init=10)
+    k_means = KMeans(n_clusters=n, init='k-means++', n_init=10)
 
     t0 = time.time()
     k_means.fit(data)
     t1 = time.time() - t0
 
-    with open('kmean.csv', mode='w') as timing:
-        timing.write('{},'.format(t1))
+    with open('kmeans.csv', mode='a') as timing:
+        timing.write('{},{}\n'.format(n, t1))
 
         print n, t1
         return t1
 
 
-if sys.argv[1]:
-    end = int(sys.argv[1])
-else:
-    end = 10
+start, end, step, timeout = 1, 10, 1, 10
+if args.start:
+    start = int(args.start)
+if args.stop:
+    end = int(args.stop)
+if args.step:
+    step = int(args.step)
+if args.timeout:
+    timeout = float(args.timeout)
 
-if sys.argv[2]:
-    step = int(sys.argv[2])
-else:
-    step = 1
 
+with open('kmeans.csv', mode='w') as timing:
+    timing.write('clusters,seconds\n')
 
 for n in range(1, end, step):
-    # Start bar as a process
     p = multiprocessing.Process(target=kmean_fit_timing, args=(data, n))
     p.start()
-
-    # Wait for 10 seconds or until process finishes
-    p.join(5)
-
-    # If thread is still active
+    p.join(timeout)
     if p.is_alive():
-        print "running... let's kill it..."
-
-        # Terminate
+        print "Ending process at {} seconds".format(timeout)
         p.terminate()
         p.join()
         break
